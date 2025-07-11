2c4dfed0007fb7a6337d754ebcf16e12293884de
unknown <sumangadaili1212@gmail.com>
2025-07-11 13:47:41 +0545

    day3

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+
+
+age = int(input("enter your age :"))
+
+
+if age ==18:
+    print("you are okay to drive")
+elif age < 30:
+    print("please use the driving saftey guide")
+elif age > 30:
+    print("you are greater than 30")
+else:
+    print("you are not ok to drive")
+
+    
+ 
---
+++ loops.py
@@ -0,0 +1,12 @@
+
+# and and or 
+
+
+#for and while loops 
+
+fruits=["mango","apple","orange","grape","kiwi",and "peache"]
+for i in range (1,9):
+    print(i)
+
+for fruit in fruits:
+    print(fruit)
